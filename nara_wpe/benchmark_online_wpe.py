28b00c15cc81ab1a7212b6b5eb2d7373f1a1bcfb
Lukas Drude <mail@lukas-drude.de>
2018-05-23 16:12:41 +0200

    Reformat online wpe benchmark

--- nara_wpe/benchmark_online_wpe.py
+++ nara_wpe/benchmark_online_wpe.py
@@ -1,24 +1,28 @@
-import tensorflow as tf
-from nara_wpe import tf_wpe
-import numpy as np
+from itertools import product
+
 import pandas as pd
+import tensorflow as tf
+
+from nara_wpe import tf_wpe
 
 benchmark = tf.test.Benchmark()
 configs = []
 delay = 1
 
 
-def gen_iter():
-    for K in range(1, 11):
-        for num_mics in range(2, 11):
-            for frame_size in [512, 1024]:
-                for dtype in [tf.complex64, tf.complex128]:
-                    for device in ['/cpu:0', '/gpu:0']:
-                        yield K, num_mics, frame_size, dtype, device
+def config_iterator():
+    return product(
+        range(1, 11),  # K
+        [2, 4, 8],  # num_mics # range(2, 11)
+        [512, 1024],  # frame_size
+        [tf.complex64, tf.complex128],  # dtype
+        ['/cpu:0']  # device # '/gpu:0'
+    )
 
 
-print('Generating configs...')
-for K, num_mics, frame_size, dtype, device in gen_iter():
+if __name__ == '__main__':
+    print('Generating configs...')
+    for K, num_mics, frame_size, dtype, device in config_iterator():
         inv_cov_tm1 = tf.eye(
             num_mics * K, batch_shape=[frame_size // 2 + 1], dtype=tf.complex64)
         filter_taps_tm1 = tf.zeros(
@@ -38,28 +42,29 @@ for K, num_mics, frame_size, dtype, device in gen_iter():
                     0.9999, K, delay)
             ))
 
-print('Benchmarking...')
-results = []
-with tf.Session() as sess:
-    for cfg in configs:
-        print(cfg)
-        result = benchmark.run_op_benchmark(
-            sess,
-            cfg['op'],
-            min_iters=100
-        )
-        result['K'] = cfg['K']
-        result['num_mics'] = cfg['num_mics']
-        result['frame_size'] = cfg['frame_size']
-        result['device'] = cfg['device']
-        result['dtype'] = cfg['dtype'].name
-        result['fps'] = 1 / result['wall_time']
-        result['real_time_factor'] = (
-            16000 / result['frame_size']) * 4 / result['fps']
-        results.append(result)
+    print('Benchmarking...')
+    results = []
+    with tf.Session() as sess:
+        for cfg in configs:
+            print(cfg)
+            result = benchmark.run_op_benchmark(
+                sess,
+                cfg['op'],
+                min_iters=100
+            )
+            result['K'] = cfg['K']
+            result['num_mics'] = cfg['num_mics']
+            result['frame_size'] = cfg['frame_size']
+            result['device'] = cfg['device']
+            result['dtype'] = cfg['dtype'].name
+            result['fps'] = 1 / result['wall_time']
+            result['real_time_factor'] = (
+                (16000 / result['frame_size']) * 4 / result['fps']
+            )
+            results.append(result)
 
-res = pd.DataFrame(results)
-print(res)
+    res = pd.DataFrame(results)
+    print(res)
 
-with open('online_wpe_results.json', 'w') as fid:
-    res.to_json(fid)
+    with open('online_wpe_results.json', 'w') as fid:
+        res.to_json(fid)
