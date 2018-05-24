01d48a578590310de39a35e1e3054ef12fd228e0
Lukas Drude <mail@lukas-drude.de>
2018-05-24 15:35:07 +0200

    Change requirements

--- setup.py
+++ setup.py
@@ -69,7 +69,12 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'tqdm', 'librosa', 'soundfile'],
+    install_requires=[
+        'numpy',
+        'tqdm',
+        'soundfile',
+        'bottleneck',
+    ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
