d0abe87a350b3214af515b3b7b1b2839480ce854
Lukas Drude <mail@lukas-drude.de>
2018-12-03 17:42:27 +0100

    Pypi version update 0.0.3

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ st.setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',
+    version='0.0.3',
 
-    description='Toy Example for WPE',
+    description='Weighted Prediction Error for speech dereverberation',
     long_description=long_description,
 
     # The project's main homepage.
