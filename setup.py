e78890e1ef0b5cd238eca39e90fbff01d9aee996
Lukas Drude <mail@lukas-drude.de>
2019-01-16 09:39:41 +0100

    Remove numpy import from setup.py to avoid import error when not yet installed

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ https://github.com/pypa/sampleproject
 import setuptools as st
 from codecs import open
 from os import path
-import numpy
+
 
 here = path.abspath(path.dirname(__file__))
 
@@ -22,7 +22,7 @@ st.setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.3',
+    version='0.0.4',
 
     description='Weighted Prediction Error for speech dereverberation',
     long_description=long_description,
@@ -98,7 +98,6 @@ st.setup(
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     # data_files=[('my_data', ['data/data_file'])],
-    include_dirs=[numpy.get_include()],
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
