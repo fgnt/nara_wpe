7b8b6071749e82c635888c4bafe2fc929800cd43
Christoph Boeddeker <cbj@mail.uni-paderborn.de>
2022-04-17 18:28:43 +0200

    Bump version: 0.0.7 → 0.0.8

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ st.setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.7',
+    version='0.0.8',
 
     description='Weighted Prediction Error for speech dereverberation',
     long_description=long_description,
